ecd4aae0c1cc269f9b17669034abe1f16438e33c
Sivaprakashpasupathibalan <61182754+Sivaprakashpasupathibalan@users.noreply.github.com>
2020-04-15 22:58:32 +0530

    Methods Added
    SQL datatable method and locations , conversions method added

--- Program.cs
+++ Program.cs
@@ -14,17 +14,22 @@ using System.Threading;
 using System.Xml;
 using System.Xml.Linq;
 using System.IO;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace WarehouseLocator
 {
 
     class Program
     {
-        public static string SERVER = "",PASSWORD = "",USER_ID = "",GET_LOCATION = "",UP_LOCATION = "",CATALOG =""; 
+        public static string SERVER = "",PASSWORD = "",USER_ID = "",GET_LOCATION = "",UP_LOCATION = "",CATALOG ="";
+        public static string[] LOC_STR;
         static void Main(string[] args)
         {
             DBConfigXMLRead();
-            Error_log(SERVER);
+            LOC_STR = DTtoSTR(SQL_Fetch());
+            for (int i = 0; i < LOC_STR.Length; i++)
+                Console.WriteLine(LOC_STR[i]);
             Console.WriteLine("\nPress any key to continue.");
             Console.ReadKey();
         }
@@ -48,7 +53,7 @@ namespace WarehouseLocator
                 Thread.Sleep(1000);
             }
         }
-        private static void Error_log(string msg)
+        private static void Error_log(string msg)                                      //Method to write errors in text file
         {
             try
             {
@@ -62,6 +67,44 @@ namespace WarehouseLocator
                 Console.WriteLine("Error in error log" + ex.Message);
                 Thread.Sleep(1000);
             }
+        }
+        private static DataTable SQL_Fetch()                                   //Method to get the data from stored procedure and return datatable
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                string Conn = @"Data Source = " + SERVER + ";Initial Catalog = " + CATALOG + ";User id = " + USER_ID + ";password = " + PASSWORD + "";
+                SqlConnection conn = new SqlConnection(Conn);
+                SqlCommand cmd = new SqlCommand(GET_LOCATION, conn);
+                cmd.CommandType = CommandType.StoredProcedure;
+                SqlDataAdapter Locations = new SqlDataAdapter(cmd);
+                Locations.Fill(dt);
+            }
+            catch(Exception ex)
+            {
+                Error_log("Error in SQL Connection" + ex.Message);
+                Console.WriteLine("Error in SQL Connection" + ex.Message);
+                Thread.Sleep(3000);
+            }
+            return dt;
+        }
+        public static string[] DTtoSTR(DataTable dt)                                       //Method to convert datatable values to str
+        {
+            string[] LOC = new string[dt.Rows.Count];
+            try
+            {
+                for (int i = 0; i < dt.Rows.Count; i++)
+                {
+                    LOC[i] = dt.Rows[i]["BinLocationName"].ToString();
+                }
+            }
+            catch(Exception ex)
+            {
+                Error_log("Error in DTtoSTR Conversion" + ex.Message);
+                Console.WriteLine("Error in DTtoSTR Conversion" + ex.Message);
+                Thread.Sleep(3000);
+            }
+            return LOC;
         }
     }
 }
